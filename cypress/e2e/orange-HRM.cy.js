79623a5c5da5e75453b0a9ec6271947fe2f97032
Andrei <andrei.test@gmail.com>
2022-10-30 00:47:16 +0300

    UPDATE MERGE BRANCH 3 cu 2 si cu master

--- cypress/e2e/orange-HRM.cy.js
+++ cypress/e2e/orange-HRM.cy.js
@@ -7,14 +7,10 @@ describe('testing OrangeHRM site', () => {
     cy.get('[name="firstName"]').type('Cristian-Andrei');
   })
 
-<<<<<<< HEAD
   //THIS IS THE MASTER BRANCH
-  
 
-=======
   it('test new branch', () => {
     //THIS IS BRANCH-3
     //THIS IS BRANCH-2
   })
->>>>>>> branch-3
 })
